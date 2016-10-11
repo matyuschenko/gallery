36bd2408d4ba01e62263445538f5485d19777b92
Maxim Matyuschenko <matyuschenko@gmail.com>
2016-10-11 16:52:23 +0300

    small improvements in css and js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 $(window).on('load', function() {
 
-	var gallery = $('.gallery__image');
-	$('.content').width(gallery.width());
+	var gallery__image = $('.gallery__image'),
+        buttons = $('.button'),
+        pics_paths = {
+            'По полу': 'pics/2.png',
+            'По возрасту': 'pics/3.png'
+        };
+
+	$('.content').width(gallery__image.width());
 
 	// BUTTONS BEHAVIOR
-	var pics_paths = {
-	    'По полу': 'pics/2.png',
-	    'По возрасту': 'pics/3.png'
-	};
-
-	var buttons = $('.button').click(function() {
+	buttons.click(function() {
 	    buttons.removeClass('button_pressed');
-	    buttons.addClass('button_clickable');
 	    $(this).addClass('button_pressed');
-	    $(this).removeClass('button_clickable');
-        gallery.attr('src', pics_paths[this.textContent]);
+        gallery__image.attr('src', pics_paths[this.textContent]);
 	});
 });
