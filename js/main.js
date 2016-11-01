4ffa2371d65abc1f63c847cfb8267cfd1cec2b3b
Maxim Matyuschenko <matyuschenko@gmail.com>
2016-11-01 18:55:39 +0300

    reorganized folder, removed unused files

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ $(window).on('load', function() {
         buttons.removeClass('button_pressed');
         $this.addClass('button_pressed');
         $('.gallery__image').attr('src', $this.data().img);
+
+        if ($this.data().subheader) {
+            $('.subheader').text($this.data().subheader);
+        }
     });
 
     buttons.eq(0).click();
